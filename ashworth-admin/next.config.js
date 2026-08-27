63e887cd4deabc3c0e7700a56c3ecf484a490877
Yugal Raj Vimal <yugalrajvimal@Yugals-MacBook-Air.local>
2026-08-28 01:33:18 +0530

    Initial Commit

--- ashworth-admin/next.config.js
+++ ashworth-admin/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // Enable full static export
 };
 
 module.exports = nextConfig;
--- ashworth/next.config.js
+++ ashworth/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Enable Next.js static export
 };
 
 module.exports = nextConfig;
